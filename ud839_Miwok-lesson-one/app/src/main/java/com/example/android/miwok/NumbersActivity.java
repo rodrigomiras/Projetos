6c34ff1f9f12a4fd80e60fa13c4105725fce1152
rodrigomiras <rodrigofmiras@gmail.com>
2016-11-16 23:39:50 -0200

    Configurado Arrays Adapters em NumbersActivity

--- ud839_Miwok-lesson-one/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-lesson-one/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,14 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -25,16 +33,45 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[10];
-        words[0] = "One";
-        words[1] = "Two";
-        words[2] = "Three";
-        words[3] = "Four";
-        words[4] = "Five";
-        words[5] = "Six";
-        words[6] = "Seven";
-        words[7] = "Eight";
-        words[8] = "Nine";
-        words[9] = "Ten";
+        //String[] words = new String[10];
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nive");
+        words.add("Ten");
+        words.add("Eleven");
+        words.add("Twelve");
+        words.add("Thirteen");
+        words.add("Fourteen");
+        words.add("Fiveteen");
+        words.add("Sixteen");
+        words.add("Seventeen");
+        words.add("Eighteen");
+        words.add("Nineteen");
+        words.add("Twenty");
+
+        /*int n = 0;
+        while (n < words.size()) {
+            Log.v("NumbersActivity", "Word at index " + n + ": " + words.get(n));
+            n++;
+        }
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for(int index = 0; index < words.size(); index++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+        }*/
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+        //GridView gridView = (GridView) findViewById(R.id.gridview);
+        //gridView.setAdapter(itemsAdapter);
     }
 }
