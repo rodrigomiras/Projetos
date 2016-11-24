d98562471a9387e83617c28144b161f647894074
rodrigomiras <rodrigofmiras@gmail.com>
2016-11-24 17:31:54 -0200

    Arquivo de audio adicionado

--- MusicPlayer/app/src/main/java/com/example/rfm/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/rfm/musicplayer/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.rfm.musicplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import static android.R.attr.onClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.the_walking_dead_abertura);
 
         Button playButton = (Button)findViewById(R.id.button_play);
         Button pauseButton = (Button)findViewById(R.id.button_pause);
@@ -21,14 +23,16 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Play apertado !", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(MainActivity.this, "Play apertado !", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
             }
         });
 
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Pause apertado !", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "Pause apertado !", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
             }
         });
     }
