9e8727914fa33b1f66c7a848851bbda395a4b38b
rodrigomiras <rodrigofmiras@gmail.com>
2016-11-28 14:22:08 -0200

    App Miwok finalizado

--- ud839_Miwok-lesson-one/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-lesson-one/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,23 +29,24 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("One","Lutti",R.drawable.number_one));
-        words.add(new Word("Two","otiiko",R.drawable.number_two));
-        words.add(new Word("three","tolookosu",R.drawable.number_three));
-        words.add(new Word("Four","oyyisa",R.drawable.number_four));
-        words.add(new Word("Five","massokka",R.drawable.number_five));
-        words.add(new Word("Six","temmokka",R.drawable.number_six));
-        words.add(new Word("Seven","kenekaku",R.drawable.number_seven));
-        words.add(new Word("Eight","kawinta",R.drawable.number_eight));
-        words.add(new Word("Nine","wo’e",R.drawable.number_nine));
-        words.add(new Word("Ten","na’aacha",R.drawable.number_ten));
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("One","Lutti",R.drawable.number_one,R.raw.number_one));
+        words.add(new Word("Two","otiiko",R.drawable.number_two,R.raw.number_two));
+        words.add(new Word("three","tolookosu",R.drawable.number_three,R.raw.number_three));
+        words.add(new Word("Four","oyyisa",R.drawable.number_four,R.raw.number_four));
+        words.add(new Word("Five","massokka",R.drawable.number_five,R.raw.number_five));
+        words.add(new Word("Six","temmokka",R.drawable.number_six,R.raw.number_six));
+        words.add(new Word("Seven","kenekaku",R.drawable.number_seven,R.raw.number_seven));
+        words.add(new Word("Eight","kawinta",R.drawable.number_eight,R.raw.number_eight));
+        words.add(new Word("Nine","wo’e",R.drawable.number_nine,R.raw.number_nine));
+        words.add(new Word("Ten","na’aacha",R.drawable.number_ten,R.raw.number_ten));
 
         WordAdapter adapter = new WordAdapter(this,words,R.color.category_numbers);
 
@@ -52,8 +54,11 @@ public class NumbersActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(NumbersActivity.this,"Item clicado",Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Word word = words.get(position);
+                //Toast.makeText(NumbersActivity.this,"Item clicado",Toast.LENGTH_SHORT).show();
+                mMediaPlayer = MediaPlayer.create(NumbersActivity.this,word.getAudioResourceId());
+                mMediaPlayer.start();
             }
         });
     }
--- ud839_Miwok-lesson-one/app/src/main/java/com/example/android/miwok/Word.java
+++ ud839_Miwok-lesson-one/app/src/main/java/com/example/android/miwok/Word.java
@@ -11,16 +11,19 @@ public class Word {
     private String mMiwokTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
     private static final int NO_IMAGE_PROVIDED = -1;
+    private int mAudioResourceId;
 
-    public Word (String defaultTranslation, String miwokTranslation){
+    public Word (String defaultTranslation, String miwokTranslation, int audioResourceId){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
     }
 
-    public Word (String defaultTranslation, String miwokTranslation, int imageResourceId){
+    public Word (String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
     public String getDefaultTranslation(){
@@ -37,5 +40,9 @@ public class Word {
 
     public boolean hasImage(){
         return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId(){
+        return mAudioResourceId;
     }
 }
